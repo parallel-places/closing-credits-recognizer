80a54a499840b964c86954b8c223cfa7c34822f0
Farbod Faghihi <farbodf@users.noreply.github.com>
2019-06-04 16:10:05 +0300

    Detect credits when they appear on the sides

--- scripts/closing_credit_recognizer.py
+++ scripts/closing_credit_recognizer.py
@@ -10,6 +10,8 @@ tf.logging.set_verbosity(tf.logging.ERROR)
 logging.basicConfig(level=logging.INFO, format='%(name)s:%(levelname)s: %(message)s')
 LOGGER = logging.getLogger("ClosingCredits")
 STARTING_POINT = 0.8
+ACCEPTANCE_THRESHOLD = 0.9
+AREAS = 3
 
 if len(sys.argv) < 2:
     LOGGER.error("Missing arguments! You should provide to arguments to the script. First, path to the video and then path to the model.")
@@ -20,19 +22,18 @@ model_path = sys.argv[2]
 
 model = models.load_model(model_path) # loading the pretrained model
 metadata = [] # Contains the timestamp (in milliseconds) and frame ID of all frames fed into the model
-frames = [] # Contains the frames themselves
 
 capture = cv2.VideoCapture(video_path)
 
 width = capture.get(3)
 height = capture.get(4)
-cutoff = int((width - height)/2)
+cutoff = int((width - height)/(AREAS-1))
 frame_rate = capture.get(5)
 total_frames = capture.get(7)
-capture.set(1, int(total_frames*STARTING_POINT))
 
-LOGGER.info(f"Movie metadata - width: {width}, height: {height}, framerate: {frame_rate}"
-            f"total_frames: {total_frames} currentframe: {capture.get(1)}")
+
+LOGGER.info(f"Movie metadata - width: {width}, height: {height}, framerate: {frame_rate}, "
+            f"total_frames: {total_frames}, currentframe: {capture.get(1)}")
 
 def get_starting_index(estimates, window_size=50):
     window = np.zeros((window_size,))
@@ -41,35 +42,43 @@ def get_starting_index(estimates, window_size=50):
         if count == 10:
             return index
         ratio_in_window = np.sum(estimates[i:(i+window_size)] == window)/window_size
-        if ratio_in_window > 0.9:
+        if ratio_in_window > ACCEPTANCE_THRESHOLD:
             if count == 0:
                 index = i
             count += 1
         else:
             count = 0
             index = None
-    return None
+    return estimates.shape[0]-1
 
-
-while(capture.isOpened()):
-    frame_info = {"time_progress": capture.get(0),
-                  "frame_id": capture.get(1)}
-    ret, frame = capture.read()
-    if ret != True:
-        break
-    if frame_info['frame_id'] % math.floor(frame_rate/10) == 0:
-        metadata.append(frame_info)
-        frame = frame[:, cutoff:-cutoff, :]
-        frame = cv2.resize(frame, (224, 224))/255.0
-        frames.append(frame)
+frame_set = []
+for i in range(AREAS):
+    frames = []
+    capture.set(1, int(total_frames*STARTING_POINT))
+    while(capture.isOpened()):
+        frame_info = {"time_progress": capture.get(0),
+                      "frame_id": capture.get(1)}
+        ret, frame = capture.read()
+        if ret != True:
+            break
+        if frame_info['frame_id'] % math.floor(frame_rate/10) == 0:
+            metadata.append(frame_info)
+            step = i*cutoff
+            frame = frame[:, step:int(height)+step, :]
+            frame = cv2.resize(frame, (224, 224))/255.0
+            frames.append(frame)
+    frame_set.append(frames)
 
 LOGGER.info("finished preprocessing frames.")
-
-frames = np.array(frames)
 capture.release()
 
-prediction_classes = model.predict_classes(frames)
-estimates = np.array([x[0] for x in prediction_classes])
+estimates_set = []
+for i in range(AREAS):
+    frames = np.array(frame_set[i])
+    prediction_classes = model.predict_classes(frames)
+    estimates = np.array([x[0] for x in prediction_classes])
+    estimates_set.append(estimates)
+
 LOGGER.info("prediction stage is done.")
 
 def zero_pad(variable):
@@ -78,7 +87,7 @@ def zero_pad(variable):
     else:
         return str(int(variable))
 
-credits_info = metadata[get_starting_index(estimates)]
+credits_info = metadata[min([get_starting_index(estimates) for estimates in estimates_set])]
 hour = credits_info['time_progress']//3600000
 minute = (credits_info['time_progress'] - hour * 3600000)//60000
 second = (credits_info['time_progress'] - hour * 3600000 - minute * 60000)//1000
